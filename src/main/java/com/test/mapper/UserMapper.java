37bbbd5d55a7277a4bdb4fa844586eed1a48a2f4
shenjia <shenjia@bjrrtx.com>
2021-05-18 09:37:27 +0800

    init

---
+++ src/main/java/com/test/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.test.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.test.entity.User;
+
+/**
+ * ClassName : UserMapper<br>
+ * Description : TODO<br>
+ *
+ * @author : sj
+ * @version : 1.0.0
+ * @date : 2021/5/15
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
